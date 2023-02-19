922645ea2ceee18bb32b877db664c3e5a1d94fb7
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-02-19 17:55:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
-
-
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
