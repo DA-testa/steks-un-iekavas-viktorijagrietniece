7a04296399733d755661ff25e85d7a5cc709d8fa
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-02-19 17:42:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i+1))
             
         if next in ")]}":
-            if not opening_brackets_stack:
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
             top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next):
-                return i + 1
      if opening_brackets_stack:
          return opening_brackets_stack[0].position
      return "Success"
@@ -28,18 +26,15 @@ def find_mismatch(text):
 
 
 def main():
-    text = input()
-    if text == 'F';
-    filename = input("")
-    with open(filename, 'r') as f:
-        text = file.read()strip()
-    elif text == 'I':
-        text = input("")
-    else:
-        print("Invalid choice")
-        return
+    izv = input()
+    if "I" in text:
+        text = input()
     mismatch = find_mismatch(text)
-    print(mismatch)
+    if mismatch == "Success":
+        print("Success")
+    else
+        print(mismatch)
+        
    
 
 
