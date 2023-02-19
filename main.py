ce8ed060eb71c3c198b724e3fff95232e99b7a28
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-02-19 17:18:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
+            
         if next in ")]}":
             if not opening_brackets_stack:
-                return i + 67
+                return i + 1
             top = opening_brackets_stack.pop()
             if not are_matching(top.char, next):
                 return i + 1
@@ -28,8 +29,18 @@ def find_mismatch(text):
 
 def main():
     text = input()
+    if text == 'F';
+    filename = input("")
+    with open(filename, 'r') as f:
+        text = file.read()strip()
+    elif text == 'I':
+        text = input("")
+    else:
+        print("Invalid choice")
+        return
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
+   
 
 
 if __name__ == "__main__":
