85126ff73770589db5384d6d207f7d613f87609a
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-02-19 18:16:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def find_mismatch(text):
             
     if opening_brackets_stack:
          return opening_brackets_stack[0].position
-     return "Success"
+    return "Success"
             
 
 
